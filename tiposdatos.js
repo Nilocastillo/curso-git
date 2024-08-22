c060739f8de395a12b401841f28bad1792add145
Nilo <nilocastillloumeres@gmail.com>
2024-08-22 13:02:16 -0500

    primer commit

---
+++ tiposdatos.js
@@ -0,0 +1,17 @@
+//tipos de datos
+//comentario una sola linea
+/*comentario
+en varias lineas*/
+let numero;
+const numeroa = 1;
+
+console.log("numeroa");
+console.log(typeof "numeroa");
+
+//ejercicios con operadores
+
+let suma, resta, multiplicacion, division;
+
+let a = 6;
+let b = 7;
+console.log(a !== b && b > a);
